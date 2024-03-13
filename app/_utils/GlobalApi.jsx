48459faa38ad314d81cab66a6cef31f484088a0e
Chetan Bargal <chetan.bargal11@gmail.com>
2024-03-13 14:16:02 +0530

    update globlaApi.jsx file

--- app/_utils/GlobalApi.jsx
+++ app/_utils/GlobalApi.jsx
@@ -3,10 +3,10 @@ const { default: axios } = require("axios");
 const API_KEY = process.env.NEXT_PUBLIC_STRAPI_API_KEY;
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:1337/api",
+  baseURL: "https://doctor-appointment-booking-admin.onrender.com/api",
   headers: {
     Authorization: `Bearer ${API_KEY}`,
-  },
+  }, 
 });
 
 const getCategory = () => axiosClient.get("catogories?populate=*");
